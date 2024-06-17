d60f921c1034055bfdf7051947db59373c2aa10f
Gaurav2418 <123626617+Gaurav2418@users.noreply.github.com>
2024-06-18 01:43:52 +0530

    logo replaced

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ export default function Header(){
     return(
         <>
         <div className="flex sm:ml-3 space-x-8 text-xl">
-            <Link to="/"><img className=" min-w-[100px] sm:w-[120px] mt-2 cursor-pointer" src="./src/assets/movieLogo.png" alt="Logo" /></Link>
+            <Link to="/">
+            <h3 className="text-2xl font-medium italic mt-2.5 cursor-pointer bg-gradient-to-r from-indigo-500 rounded-md pl-2 pb-1">GStore movie</h3>
+            {/* <img className=" min-w-[100px] sm:w-[120px] mt-2 cursor-pointer" src="./src/assets/movieLogo.png" alt="Logo" /> */}
+            </Link>
             <Link  to="/movies/popular" className="flex pt-4">Popular</Link>
             <Link to="/movies/top_rated" className="flex pt-4">Top Rated</Link>
             <Link to="/movies/upcoming" className="flex pt-4">Upcoming</Link>
