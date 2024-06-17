69be89f779fddbe7524d665532502e3d38723d62
Gaurav2418 <123626617+Gaurav2418@users.noreply.github.com>
2024-06-18 01:47:20 +0530

    logo updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
         <>
         <div className="flex sm:ml-3 space-x-8 text-xl">
             <Link to="/">
-            <h3 className="text-2xl font-medium italic mt-2.5 cursor-pointer bg-gradient-to-r from-indigo-500 rounded-md pl-2 pb-1">GStore movie</h3>
+            <h3 className="text-2xl font-medium italic mt-2.5 cursor-pointer bg-gradient-to-r from-indigo-500 rounded-md pl-2 pb-1 pt-1">GStore movies</h3>
             {/* <img className=" min-w-[100px] sm:w-[120px] mt-2 cursor-pointer" src="./src/assets/movieLogo.png" alt="Logo" /> */}
             </Link>
             <Link  to="/movies/popular" className="flex pt-4">Popular</Link>
